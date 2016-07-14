f0e5bd8272118ddc47100e9b2751fc5b04371629
AsukaNakagawa <nina-jamie.elle14@live.jp>
2016-07-14 12:40:29 -0700

    done step2

---
+++ LHL-June.CoverFlowLayout/CoverFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  CoverFlowLayout.h
+//  LHL-June.CoverFlowLayout
+//
+//  Created by Asuka Nakagawa on 2016-07-14.
+//  Copyright © 2016 Asuka Nakagawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoverFlowLayout : UICollectionViewFlowLayout
+
+@end
---
+++ LHL-June.CoverFlowLayout/CoverFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  CoverFlowLayout.m
+//  LHL-June.CoverFlowLayout
+//
+//  Created by Asuka Nakagawa on 2016-07-14.
+//  Copyright © 2016 Asuka Nakagawa. All rights reserved.
+//
+
+#import "CoverFlowLayout.h"
+
+@implementation CoverFlowLayout
+
+@end
