85f9203e4aa925ded5738698d3e54ffddb6fc983
AsukaNakagawa <nina-jamie.elle14@live.jp>
2016-07-14 12:27:32 -0700

    June. My initial commit

---
+++ LHL-June.CoverFlowLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHL-June.CoverFlowLayout
+//
+//  Created by Asuka Nakagawa on 2016-07-14.
+//  Copyright © 2016 Asuka Nakagawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHL-June.CoverFlowLayout/CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  LHL-June.CoverFlowLayout
+//
+//  Created by Asuka Nakagawa on 2016-07-14.
+//  Copyright © 2016 Asuka Nakagawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+@end
